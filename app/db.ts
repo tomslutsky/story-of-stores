6ef2487f9ecc9f5ae28d32a5f184b4294fe2fca7
Tom Slutsky <toms@loox.io>
2025-02-22 16:22:38 +0200

    wip

--- app/db.ts
+++ app/db.ts
@@ -1,10 +1,11 @@
-export default {
+const db = {
   "space-rocks": {
     title: "Space Rocks",
     image: "/space-rocks.png",
     description:
       " These space rocks are out of this world! Perfect for snacking on during your next space adventure.",
     price: 29.99,
+    slug: "space-rocks",
   },
   "inter-galactic-jello": {
     title: "Inter Galactic Jello",
@@ -12,5 +13,10 @@ export default {
     description:
       "This jello is so good, it's out of this world! Perfect for snacking on during your next space adventure.",
     price: 19.99,
+    slug: "inter-galactic-jello",
   },
 };
+
+export async function getProduct(slug: string) {
+  return db[slug as keyof typeof db];
+}
