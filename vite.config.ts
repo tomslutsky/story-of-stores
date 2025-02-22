87f339e74354d368d275cbfeaed4490930a01aa5
Tom Slutsky <toms@loox.io>
2025-02-22 15:38:31 +0200

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  plugins: [tailwindcss(), tsconfigPaths()],
+  plugins: [tailwindcss(), vue(), tsconfigPaths()],
 });
