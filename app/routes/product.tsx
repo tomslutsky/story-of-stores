6d7484a5486f54b80d332a0bc8834242c50e4f89
Tom Slutsky <toms@loox.io>
2025-02-22 16:08:07 +0200

    wip

--- app/routes/product.tsx
+++ app/routes/product.tsx
@@ -2,6 +2,7 @@ import { getProduct } from "~/db";
 import { RatingWidget } from "~/components/rating-widget";
 import { useLoaderData } from "react-router";
 import type { LoaderFunctionArgs } from "react-router";
+import { useState } from "react";
 
 export async function loader({ params: { slug } }: LoaderFunctionArgs) {
   return await getProduct(slug!);
@@ -9,6 +10,8 @@ export async function loader({ params: { slug } }: LoaderFunctionArgs) {
 
 export function Component() {
   const { title, image, description, price } = useLoaderData();
+  const [ratings, setRatings] = useState<number[]>([]);
+
   return (
     <div className="product">
       <div className="product-image">
@@ -17,14 +20,23 @@ export function Component() {
       <div className="product-info">
         <span className="product-title">
           <h2>{title}</h2>
+          <span>({avg(ratings).toFixed(1)})</span>
         </span>
         <p className="price">${price}</p>
         <p className="description">{description}</p>
         <button className="buy-button">Add to Cart</button>
         <div className="rating-section">
-          <RatingWidget />
+          <RatingWidget
+            raters={ratings.length}
+            onRatingAdded={(rating) => setRatings((prev) => [...prev, rating])}
+          />
         </div>
       </div>
     </div>
   );
+}
+
+function avg(arr: number[]) {
+  if (!arr.length) return 0;
+  return arr.reduce((acc, curr) => acc + curr, 0) / arr.length;
 }
